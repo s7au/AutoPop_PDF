1e284bec75be2c87bef551b055d0666a70cadb6a
Shawn Au <s7au@uwaterloo.ca>
2015-10-05 15:43:47 -0400

    Now produces PDF's based on text input

--- PopulateFields.php
+++ PopulateFields.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 
-<form method="post" action= "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" >
+<form method="post" action= "<?php echo htmlspecialchars("merge.php");?>" target="_blank">
 First_Name:<br>
 <input type="text" name="First_Name">
 <br>
@@ -54,22 +54,6 @@ PDF:<br>
 <br><br>
 <input type="submit" value="Submit">
 </form>
-
-<?php
-require_once('merge.php');
-// if(isset$_POST['Name']) {
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$pathtoinput = array (
-		$First_Name => $_POST['First_Name'],
-		$Last_Name => $_POST['Last_Name'],
-		$Street => $_POST['Street'],
-		$City => $_POST['City'],
-	    $Phone => $_POST['Phone'],
-	    );
-	pageToPDF($pathToFile, $pathToTemplate, $pathToInput);
-}
-?>
 
 </body>
 </html>
--- merge.php
+++ merge.php
@@ -7,11 +7,11 @@ require_once('pageText.php');
 
 $pathToFile = '/var/www/subdoc2.pdf';
 $pathToTemplate = '/var/www/html/AutoPop_PDF/InputTemplateTest.txt';
-$pathToInput = array(
-    "Name" => "Shawn Au",
-    "Address" => "46 WestBrook Drive",
-    "Phone" => "7809053448",
-    );
+// $pathToInput = array(
+//     "Name" => "Shawn Au",
+//     "Address" => "46 WestBrook Drive",
+//     "Phone" => "7809053448",
+//     );
 
 class concat_pdf extends FPDI {
     var $files;
@@ -48,9 +48,19 @@ function pageToPDF($page, $template, $text) {
     $pdf->setFiles($page); //$files is an array with existing PDF files.
     $pdf->setInput($text,$template);
     $pdf->concat();
-    $pdf->Output('boo.pdf','D');
+    $pdf->Output('boo.pdf','I');
 }
 
-pageToPDF($pathToFile, $pathToTemplate, $pathToInput);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $pathToInput = array (
+        "First_Name" => $_POST['First_Name'],
+        "Last_Name" => $_POST['Last_Name'],
+        "Street" => $_POST['Street'],
+        "City" => $_POST['City'],
+        "Phone" => $_POST['Phone'],
+        );
+    // var_dump($pathToInput);
+    pageToPDF($pathToFile, $pathToTemplate, $pathToInput);
+}
 
 ?>
